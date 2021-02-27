3faa0579c994d7dec527aaa3327a5aa07b8885df
Leifhaa <leifhaa@hotmail.com>
2021-02-27 19:09:23 +0100

    Added comments.

--- scripts/terraform-apply.sh
+++ scripts/terraform-apply.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ex
+#project id is set in travis file
 export TF_VAR_project_id=$GCP_PROJECT_ID
+#machine type is set on travis by command 'travis set env ...'
 export TF_VAR_machine_type=$TF_ENV_machine_type
 terraform init
 terraform plan
