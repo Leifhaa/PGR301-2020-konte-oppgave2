82560f2fa5e7ec3de6b1e268f10b9417a39eee8c
Leifhaa - Ubuntu <Leifhaa@hotmail.com>
2021-02-28 17:09:22 -0500

    Fixed linux script

--- init/scripts/deploy-linux.sh
+++ init/scripts/deploy-linux.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ -z "$TEST_DIR" ]
-then
-    echo "TEST_DIR was not set, please enter the path: "
-    read input_variable
-    export TEST_DIR=$input_variable
-fi
+echo "Please enter an bucket name: "
+read var1
+echo "Please enter your google project id: "
+read var2
+cd ..
+export gcp_bucket_name=$var1
+export gcp_project_id=$var2
+echo $var1
+echo $var2
+export TF_VAR_bucket_name=${gcp_bucket_name}
+export TF_VAR_project_id=${gcp_project_id}
+terraform init
+terraform apply
+
