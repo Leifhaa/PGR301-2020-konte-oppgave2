6c9bbb131a76ad3b2dc081e118cff3cb8cd41abe
Leifhaa <leifhaa@hotmail.com>
2021-02-28 23:44:27 +0100

    Updated bucket name.

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   #Save the terraform state (backend) to google cloud storage (gcs)
   backend "gcs" {
-    bucket = "leifhaa-env-bucket-0"
+    bucket = "leifhaa-my-terraform-bucket-0"
     #Prefix inside the bucket. Will create a workspace with such name
     prefix = "terraformstate"
     credentials = "terraform_keyfile.json"
