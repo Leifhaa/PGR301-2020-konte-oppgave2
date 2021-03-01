1de28afd81dd6c76c9aa2c8034ce333e0d25050f
Leifhaa <leifhaa@hotmail.com>
2021-03-01 02:28:02 +0100

    Added a random change to ensure travis only builds on master.

--- variables.tf
+++ variables.tf
@@ -7,3 +7,8 @@ variable "machine_type" {
   description = "Specifies type of machine in google compute"
   type = string
 }
+
+variable "foo_variable" {
+  description = "Variable is made to make a git push to ensure travis only builds infrastructure on master branch."
+  type = string
+}
